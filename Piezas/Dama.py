67e779597da7aaec51348bb6d73c4adfc08bbd57
Pau Castro Sampol <paucastrosampol@gail.com>
2018-01-24 12:52:35 +0100

    dama

--- Piezas/Dama.py
+++ Piezas/Dama.py
@@ -30,10 +30,22 @@ class Dama:
         else :
             self.position = x
     
+    # reset all booleans of moves to 1
+    def resetMoves(self) :
+        self.DA = 1 #Represents the down-left Diagonal: 0->obstacles 1->no obstacle
+        self.DB = 1 #Represents the down-rigth Diagonal
+        self.DC = 1 #Represents the up-left Diagonal
+        self.DD = 1 #Represents the up-rigth Diagonal
+        self.VA = 1 #Represents the down Vertical
+        self.VB = 1 #Represents the up Vertical
+        self.HA = 1 #Represents the left Horitzontal
+        self.HB = 1 #Represents the rigth Horitzontal
+        
     #x --> position of the piece in format (1x128)
     def movePiece(self, x):
         self.position = x
-        self.posibleMoves()
+        self.resetMoves()
+        self.posibleMoves(1)
     
     #piezas --> position of all pieces in format (1x128)
     def setPiezas(self, piezas):
@@ -61,10 +73,24 @@ class Dama:
             if self.DA:
                 positions.append(self.position - (x*16) - x) 
             x = x + 1
+            self.checkAvaliableMoves(x)
         self.moves = positions
     
     # The function will check if the moves are avaliable with the rest of the pieces
     def checkAvaliableMoves(self, x) :
         if self.HB and self.piezas.contains(self.position + x) :
             self.HB = 0
-        
+        if self.VB and self.piezas.contains(self.position + (x*16)) :
+            self.VB = 0
+        if self.DD and self.piezas.contains(self.position + (x*16) + x) :
+            self.DD = 0
+        if self.DC and self.piezas.contains(self.position + (x*16) - x) :
+            self.DC = 0
+        if self.HA and self.piezas.contains(self.position - x) :
+            self.HA = 0
+        if self.VB and self.piezas.contains(self.position - (x*16)) :
+            self.VB = 0
+        if self.DB:
+                positions.append(self.position - (x*16) + x)
+            if self.DA:
+                positions.append(self.position - (x*16) - x)
